3adb1ebf293c1f7d30d83015a93209e00d8ac882
gyfydkf <111689153+gyfydkf@users.noreply.github.com>
2026-04-01 21:33:19 +0800

    Fix launch_sim parameters for camera_frame

--- launch/linklauncher.launch.py
+++ launch/linklauncher.launch.py
@@ -4,18 +4,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 1. 摄像机光学坐标系变换 (camera_link -> camera_optical_frame)
-        # 作用：连接机器人本体坐标系和视觉算法坐标系
-        # 变换：位置重合 (0 0 0)，旋转 (Yaw=-90, Pitch=0, Roll=-90) 使得 Z轴向前, Y轴向下
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='camera_optical_tf',
-            # 参数格式: x y z yaw pitch roll frame_id child_frame_id
-            arguments=['0', '0', '0', '-1.5708', '0', '-1.5708', 'camera_link', 'camera_optical_frame']
-        ),
-
-        # 2. 发射器坐标系 (map -> launcher_link)
+        # 1. 发射器坐标系 (map -> launcher_link)
         # 作用：模拟一个架设在地上，枪口抬高 10 度的发射器
         # 变换：假设高度 1.0m，Pitch = -10度 (-0.1745 rad) 实现仰角
         Node(
